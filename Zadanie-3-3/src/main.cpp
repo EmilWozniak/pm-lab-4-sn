d55af79e4acb761072fbc72695b5f7b9c3f37a94
Emil Woźniak <emil.wozniak01@student.wat.edu.pl>
2021-01-11 14:45:46 +0100

    Wykonano zadanie dodatkowe 3.3.3

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,39 +1,27 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <stdio.h>
 
 // Inicjalizacja obiektu klasy LiquidCrystal: RS, E, DB4, DB5, DB6, DB7
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-char tablica [] = "Emil";
-int tabLen = 0;
-
-void lcdClear(void);
+char rxBuff[32];
+int ValueA = 0, ValueB = 0;
+char operation[16];
 
 void setup() {
     lcd.begin(16, 2); //Ustawienie liczby kolumn i wierszy
-    lcd.print("Emil Wozniak"); //Komunikat powitalny  
-    tabLen = sizeof(tablica);
+    lcd.print("2+2");
+    Serial.begin(9600);
+    Serial.setTimeout(3000);
     delay(2000);
 }
 
-void loop() {  
-    for (int i = 0; i < (18 - tabLen); i++){
-        lcdClear();
-        lcd.setCursor(i, 0);
-        lcd.print(tablica);
-        delay(1000);    
-    }
-
-    for (int i = (17 - tabLen); i >= 0; i--){
-        lcdClear();
-        lcd.setCursor(i, 1);
-        lcd.print(tablica);
-        delay(1000);
-    }
-}
-
-void lcdClear(void){
-    lcd.setCursor(0, 0);
-    lcd.print("................");
+void loop() {
+    Serial.readBytesUntil('\r', rxBuff, sizeof(rxBuff) - 1);
+    // Odczyt wartości i ich konwersja
+    lcd.setCursor(0, 0); //W pierwszym wierszu jest wpisany tekst
+    lcd.print(rxBuff);
     lcd.setCursor(0, 1);
-    lcd.print("................");
+    lcd.print("=");
+    // Wykonanie obliczeń i wyświetlenie wyniku
 }
