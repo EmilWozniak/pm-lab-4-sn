f945a54bfce49b273f28c88d948060a7ce708655
Emil Woźniak <emil.wozniak01@student.wat.edu.pl>
2021-01-22 13:43:37 +0100

    Wykonano zadanie dodatkowe 3.3.4 - stoper

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -2,26 +2,58 @@
 #include <LiquidCrystal.h>
 #include <stdio.h>
 
-// Inicjalizacja obiektu klasy LiquidCrystal: RS, E, DB4, DB5, DB6, DB7
+int stanPrzycisku = 0;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-char rxBuff[32];
-int ValueA = 0, ValueB = 0;
-char operation[16];
+
+int hh1, mm1, ss1, hh2, mm2, ss2;
+char czas1[16];
+char czas2[16];
 
 void setup() {
-    lcd.begin(16, 2); //Ustawienie liczby kolumn i wierszy
-    lcd.print("2+2");
-    Serial.begin(9600);
-    Serial.setTimeout(3000);
-    delay(2000);
+  lcd.begin(16, 2);
+  pinMode(7, INPUT);
 }
 
 void loop() {
-    Serial.readBytesUntil('\r', rxBuff, sizeof(rxBuff) - 1);
-    // Odczyt wartości i ich konwersja
-    lcd.setCursor(0, 0); //W pierwszym wierszu jest wpisany tekst
-    lcd.print(rxBuff);
-    lcd.setCursor(0, 1);
-    lcd.print("=");
-    // Wykonanie obliczeń i wyświetlenie wyniku
+  stanPrzycisku = digitalRead(7);
+  if (stanPrzycisku == HIGH) {
+    ss2++;
+    if (ss2 == 60)
+    {
+      ss2 = 0;
+      mm2++;
+      if (mm2 == 60)
+      {
+        mm2 = 0;
+        hh2++;
+      }
+    }
+  }
+  
+  ss1++;
+  
+  if (ss1 == 60)
+  {
+    ss1 = 0;
+    mm1++;
+    if (mm1 == 60)
+    {
+      mm1 = 0;
+      hh1++;
+    }
+  }
+  
+  lcd.setCursor(0, 0);
+  lcd.print(hh2);
+  lcd.print(" : ");
+  lcd.print(mm2);
+  lcd.print(" : ");
+  lcd.print(ss2);
+  lcd.setCursor(0, 1);
+  lcd.print(hh1);
+  lcd.print(" : ");
+  lcd.print(mm1);
+  lcd.print(" : ");
+  lcd.print(ss1);
+  delay(10);
 }
